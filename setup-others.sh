fdf5cb7cb4783949fb9a813bb628badf6b5a905a
mtlive <m.t_79@hotmail.com>
2018-08-12 00:04:18 +0430

    Create setup-others.sh

---
+++ setup-others.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+unzip platform-tools-latest-linux.zip -d ~
+PATH="$(pwd)/platform-tools:$PATH"
+mkdir -p ~/bin
+mkdir -p ~/android/lineage
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+PATH="$(pwd)/bin:$PATH"
+source ~/.profile
+cd ~/android/lineage
+repo init -u https://github.com/LineageOS/android.git -b cm-12.1
+repo sync
+source build/envsetup.sh
+
