93584f02c3db83fbc38a6137f07882061ed45ed5
mtlive <m.t_79@hotmail.com>
2018-08-12 22:52:58 +0430

    Update setup-others.sh

--- setup-others.sh
+++ setup-others.sh
@@ -13,7 +13,7 @@ repo init --depth=1 -u git://github.com/LineageOS/android.git -b cm-12.1 --quiet
 y
 y
 !
-mkdir ~/android/lineage/.repo/local_manifests
+mkdir -p ~/android/lineage/.repo/local_manifests
 curl https://raw.githubusercontent.com/mtlive/cm12.1-build-ancora/master/ancora.xml -o ~/android/lineage/.repo/local_manifests/ancora.xml
 repo init -m ancora.xml
 repo sync --quiet
