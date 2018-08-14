d9967c02de3fb876f898e632fa68ced4bd58f4d7
mtlive <m.t_79@hotmail.com>
2018-08-14 21:46:12 +0430

    Update setup-others.sh

--- setup-others.sh
+++ setup-others.sh
@@ -16,5 +16,6 @@ y
 mkdir -p ~/android/lineage/.repo/local_manifests
 curl https://raw.githubusercontent.com/mtlive/cm12.1-build-ancora/master/ancora.xml -o ~/android/lineage/.repo/local_manifests/ancora.xml
 repo sync --quiet
+find . -name "*.mk" -type f -exec sed -i 's/vendor\/cm\/config\//vendor\/cm\/config\//g' {} +
 source build/envsetup.sh 
 
