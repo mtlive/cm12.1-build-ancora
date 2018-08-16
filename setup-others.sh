af0dfde230abae90b93fd9e6be9ec7bdee2e3206
mtlive <m.t_79@hotmail.com>
2018-08-16 17:37:02 +0430

    Update setup-others.sh

--- setup-others.sh
+++ setup-others.sh
@@ -9,12 +9,12 @@ chmod a+x ~/bin/repo
 PATH="$(pwd)/bin:$PATH"
 source ~/.profile
 cd ~/android/lineage
-repo init --depth=1 -u git://github.com/LineageOS/android.git -b cm-12.1 --quiet <<!
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b cm-12.1  <<!
 y
 y
 !
 mkdir -p ~/android/lineage/.repo/local_manifests
 curl https://raw.githubusercontent.com/mtlive/cm12.1-build-ancora/master/ancora.xml -o ~/android/lineage/.repo/local_manifests/ancora.xml
-repo sync --quiet
+repo sync 
 source build/envsetup.sh 
 
