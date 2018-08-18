42d1f05b63843834a7364e4c6317afb08735719e
mtlive <m.t_79@hotmail.com>
2018-08-18 22:24:55 +0430

    Update setup-others.sh

--- setup-others.sh
+++ setup-others.sh
@@ -24,7 +24,7 @@ cd ~/android/lineage
 
 #Updating libshims
 #rm -R device/samsung/ancora/libshims/8
-svn checkout --force https://github.com/doadin/android_device_samsung_ancora_tmo/branches/cm-12.1_ion_pmem-libshim/libshims "device/samsung/ancora/libshims"
+svn export --force https://github.com/doadin/android_device_samsung_ancora_tmo/branches/cm-12.1_ion_pmem-libshim/libshims "device/samsung/ancora/libshims"
 ls
 
 breakfast ancora
