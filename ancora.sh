308181ec480e7c9f1b1f01193ac89e86f72a28d3
mtlive <m.t_79@hotmail.com>
2018-08-18 13:50:55 +0430

    Create ancora.sh

---
+++ ancora.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd ~/android/lineage
+svn checkout https://github.com/doadin/android_device_samsung_ancora_tmo/branches/cm-12.1_ion_pmem-libshim/libshims "device/samsung/ancora/libshims"
+ls
+breakfast ancora
+croot
+brunch ancora
+cd $OUT
+ls -l
