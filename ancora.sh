bff1065add7c3039b25e1fa22e889b3bf730ef25
mtlive <m.t_79@hotmail.com>
2018-08-18 13:53:40 +0430

    Update ancora.sh

--- ancora.sh
+++ ancora.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 cd ~/android/lineage
-svn checkout https://github.com/doadin/android_device_samsung_ancora_tmo/branches/cm-12.1_ion_pmem-libshim/libshims "device/samsung/ancora/libshims"
+#rm -R device/samsung/ancora/libshims/8
+svn --force checkout https://github.com/doadin/android_device_samsung_ancora_tmo/branches/cm-12.1_ion_pmem-libshim/libshims "device/samsung/ancora/libshims"
 ls
 breakfast ancora
 croot
