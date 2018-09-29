fe28d704a1a7151a5088d7562a08d7323b16c74a
mtlive <m.t_79@hotmail.com>
2018-09-29 13:54:28 +0330

    Update setup-others.sh

--- setup-others.sh
+++ setup-others.sh
@@ -10,7 +10,7 @@ chmod a+x ~/bin/repo
 PATH="$(pwd)/bin:$PATH"
 source ~/.profile
 cd ~/android/lineage
-repo init --depth=1 -u git://github.com/LineageOS/android.git --quiet -b cm-12.1  <<!
+repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git --quiet -b twrp-7.1  <<!
 y
 y
 !
@@ -46,11 +46,10 @@ git fetch --unshallow msm7x30
 git revert 14c090d 
 cd ~/android/lineage
 sed -i 's/utf16_to_utf8(str,\slen,\s(char\*)\sdata)/utf16_to_utf8(str, len, (char*) data, len + 1)/g' hardware/qcom/media-caf/msm7x30/dashplayer/DashPlayer.cpp   
-sed -i 's/#\sCamera/# Camera\'$'\nBOARD_USES_LEGACY_OVERLAY := true/g' device/samsung/ancora/BoardConfig.mk
 cp -f $BASEDIR/config.xml device/samsung/ancora/overlay/frameworks/base/core/res/res/values/
 #Using our prebuilt kernel
-curl -L https://github.com/mtlive/samsung-kernel-msm7x30-1/releases/download/untagged-f36e032f95b728827d2a/zImage -o device/samsung/ancora/zImage
-export TARGET_PREBUILT_KERNEL="device/samsung/ancora/zImage" 
+#curl -L https://github.com/mtlive/samsung-kernel-msm7x30-1/releases/download/untagged-f36e032f95b728827d2a/zImage -o device/samsung/ancora/zImage
+#export TARGET_PREBUILT_KERNEL="device/samsung/ancora/zImage" 
 
 #Revert libshims
 
