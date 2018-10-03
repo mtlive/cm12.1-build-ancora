058ff4cbac29407598fb119856d696daa1c3fb8c
mtlive <m.t_79@hotmail.com>
2018-10-03 21:23:24 +0330

    Update and apply patches for default partition style.

--- setup-others.sh
+++ setup-others.sh
@@ -34,7 +34,7 @@ git remote add ancora-fwnative git://github.com/sirmordred/android_frameworks_na
 git fetch ancora-fwnative
 git cherry-pick 10c3798 c3cda27 fd31f18 0c59f3f 
 cd ~/android/lineage/frameworks/base
-git am $BASEDIR/android_frameworks_base_simple_dialog.patch
+git am $BASEDIR/patches/android_frameworks_base_simple_dialog.patch
 cd ~/android/lineage/hardware/qcom/display-caf/msm7x30
 git fetch --unshallow msm7x30
 git revert 2bfbf21
@@ -48,11 +48,14 @@ cd ~/android/lineage
 sed -i 's/utf16_to_utf8(str,\slen,\s(char\*)\sdata)/utf16_to_utf8(str, len, (char*) data, len + 1)/g' hardware/qcom/media-caf/msm7x30/dashplayer/DashPlayer.cpp   
 sed -i 's/#\sCamera/# Camera\'$'\nBOARD_USES_LEGACY_OVERLAY := true/g' device/samsung/ancora/BoardConfig.mk
 sed -i 's/#\sTWRP\srecovery/# TWRP recovery\'$'\nRECOVERY_VARIANT := twrp/g' device/samsung/ancora/BoardConfig.mk
+sed -i 's/RECOVERY_SDCARD_ON_DATA\s:=\strue//g' device/samsung/ancora/BoardConfig.mk
 cp -f $BASEDIR/config.xml device/samsung/ancora/overlay/frameworks/base/core/res/res/values/
 rm -R prebuilts/gcc/linux-x86/arm/arm-eabi-4.8
 git clone https://bitbucket.org/UBERTC/arm-eabi-4.9 prebuilts/gcc/linux-x86/arm/arm-eabi-4.8
 rm -R prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.8
 git clone https://bitbucket.org/UBERTC/arm-linux-androideabi-4.9 prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.8
+#Switch to old partition layout
+git apply -R $BASEDIR/patches/Change_the_assignment_of_partitions_and_use_an_emulated_sdcard.patch $BASEDIR/patches/Switch_to_full_phone_config.patch --directory=~/android/lineage/device/samsung/ancora
 
 #Revert libshims
 
